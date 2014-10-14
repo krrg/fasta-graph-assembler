a4efcac2a941b6864be36c9d17be59cd079d85f0
Ken <krrgit@gmail.com>
2014-10-14 16:39:30 -0600

    lksdgjlksdjg

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ __author__ = 'krr428'
 import fasta_reader
 import seq_splitter
 from debruijn import DeBruijnGraph
+from collections import defaultdict
 import sys
 
 if __name__ == "__main__":
@@ -12,8 +13,15 @@ if __name__ == "__main__":
     complete_kmer_list = []
 
     for dnaseq in seqs:
-        kmers = seq_splitter.get_kmers_from(dnaseq, klen=8)
+        kmers = seq_splitter.get_kmers_from(dnaseq, klen=33)
         complete_kmer_list.extend(kmers)
 
-    # print "\n".join(DeBruijnGraph(complete_kmer_list).read_all_contigs())
-    print "\n".join(sorted(DeBruijnGraph(seqs).read_all_contigs()))
+    frequencies = defaultdict(int)
+
+    for kmer in complete_kmer_list:
+        frequencies[kmer] += 1
+
+    supported_kmers = filter(lambda kmer: frequencies[kmer] > 1, frequencies.iterkeys())
+
+    print "\n".join(DeBruijnGraph(set(supported_kmers)).read_all_contigs())
+    #print "\n".join(sorted(DeBruijnGraph(seqs).read_all_contigs()))
