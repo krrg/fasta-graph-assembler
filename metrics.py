2f2ffcc104c0ed1f9a10dbd7918ae6256218b67b
Ken <krrgit@gmail.com>
2014-10-14 18:44:26 -0600

    Refactored the metrics.py

--- metrics.py
+++ metrics.py
@@ -1,25 +1,48 @@
-
 import sys
+
+
+class ContigMetrics:
+
+    def __init__(self, contigs):
+        self.contigs = sorted(contigs)
+        self.contig_lengths = map(len, self.contigs)
+
+    def mean_contig_size(self):
+        return sum(self.contig_lengths) / float(len(self.contigs))
+
+    def total_contigs(self):
+        return len(self.contigs)
+
+    def largest_contig_size(self):
+        return self.contig_lengths[-1]
+
+    def n_50(self):
+        templist = []
+        for contig_len in self.contig_lengths:
+            templist.extend([contig_len]*contig_len)
+        return templist[len(templist) / 2]
+
 
 def read_input_file(filename):
     with open(filename) as f:
         return map(str.strip, f.readlines())
 
+
 if __name__ == "__main__":
     lines = read_input_file(sys.argv[1])
     
-    lenlengths = map(len, lines)
-    lenlengths = sorted(lenlengths)
+    metrics = ContigMetrics(lines)
 
-    print "Average contig size:"
-    print sum(lenlengths) / float(len(lenlengths))
-    print 
-    print "N-50"
-    print lenlengths[len(lenlengths)/2]
-    print 
-    print "Total contigs returned:"
-    print len(lines)
+    print "Average (mean) contig size:"
+    print metrics.mean_contig_size()
+    print
+    print "N-50:"
+    print metrics.n_50()
+    print
+    print "Total contigs generated:"
+    print metrics.total_contigs()
     print
     print "Largest contig size:"
-    print lenlengths[-1]
+    print metrics.largest_contig_size()
+    print
 
--- run.py
+++ run.py
@@ -22,6 +22,14 @@ def get_frequent_kmers(kmerlist, minimum_freq):
     return filter(lambda key: frequencies[key] >= minimum_freq, frequencies.iterkeys())
 
 
+def determine_out_file(infilename, klen):
+    return "outputs/" + str(infilename.split('/')[-1]).replace(".fasta", ".{0}.txt".format(klen))
+
+
+def write_output_file(contigs_result, filename):
+    with open(filename, 'w') as f:
+        f.write("\n".join(contigs_result))
+
 if __name__ == "__main__":
 
     fasta_sequences = fasta_reader.read_input_file(sys.argv[1] if len(sys.argv) > 1 else "example.data.fasta")
@@ -30,4 +38,9 @@ if __name__ == "__main__":
     complete_kmer_list = fasta_to_kmers(fasta_sequences, klen)
     supported_kmers = get_frequent_kmers(complete_kmer_list, 2)
 
-    print "\n".join(DeBruijnGraph(set(supported_kmers)).read_all_contigs())
+    contigs_result = DeBruijnGraph(set(supported_kmers)).read_all_contigs()
+    contigs_result = sorted(contigs_result, key=lambda contig: len(contig))
+
+    output_filename = determine_out_file(sys.argv[1], klen)
+    write_output_file(contigs_result, output_filename)
+    print "Output written to", output_filename
